eb564753dde30b7b09038d36176817b88658379a
dyl <51924672+dlsnyder8@users.noreply.github.com>
2023-01-22 12:15:49 -0600

    a copy and paste would've saved 3 commits

--- guildcogs/friendly.py
+++ guildcogs/friendly.py
@@ -311,7 +311,7 @@ class Friendly(commands.Cog):
     @app_checks.is_verified()
     @app_commands.command()
     @guild_only()
-    @app_commands.checks.dynamic_cooldown(custom_is_me(1,60), AppBucketType.Member)
+    @app_commands.checks.dynamic_cooldown(custom_is_me(1,60), key=AppBucketType.Member)
     @checks.server_configured()
     async def join(self, ctx):
         if ctx.author._roles.has(fly_roles[19]):
